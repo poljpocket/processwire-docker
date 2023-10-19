6f373b5d63c52c03a4c5a8933ae48dfc82cb8fa0
Julian Pollak <j.pollak@fruitcake.ch>
2023-10-19 14:39:37 +0200

    make restoring the db manual as well.

---
+++ dbrestore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# the name or id of the database container for this composition
+DB_CONTAINER=_database_container_name_or_id_
+
+# one way to get the above automatically
+# this only works if there is only one running container with 'database' in it's name
+#DB_CONTAINER=$(docker ps --filter "name=database" --format {{.ID}} --filter status=running)
+
+docker exec -i $DB_CONTAINER sh -c 'mariadb -udocker -pdocker docker' < ./data/docker-dump.sql
