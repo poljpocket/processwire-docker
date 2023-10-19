5ca2424d024a29be59efa250f493a533bcb0f3ac
Julian Pollak <j.pollak@fruitcake.ch>
2023-10-19 14:21:36 +0200

    add context for DB_CONTAINER variable

--- dbdump.sh
+++ dbdump.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# the name of the database container for this composition
-DB_CONTAINER_NAME=
+# the name or id of the database container for this composition
+DB_CONTAINER=_database_container_name_or_id_
 
-docker exec $DB_CONTAINER_NAME sh -c 'mariadb-dump -udocker -pdocker docker' > ./data/docker-dump.sql
+# one way to get the above automatically
+# this only works if there is only one running container with 'database' in it's name
+#DB_CONTAINER=$(docker ps --filter "name=database" --format {{.ID}} --filter status=running)
+
+docker exec $DB_CONTAINER sh -c 'mariadb-dump -udocker -pdocker docker' > ./data/docker-dump.sql
