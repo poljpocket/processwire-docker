964e26b44904113ef3a2dcacd1de876a30ae26be
Julian Pollak <j.pollak@fruitcake.ch>
2023-11-16 11:40:48 +0100

    update dump scripts to ignore caches table data

--- dbdump.sh
+++ dbdump.sh
@@ -7,4 +7,8 @@ DB_CONTAINER=_database_container_name_or_id_
 # this only works if there is only one running container with 'database' in it's name
 #DB_CONTAINER=$(docker ps --filter "name=database" --format {{.ID}} --filter status=running)
 
-docker exec $DB_CONTAINER sh -c 'mariadb-dump -udocker -pdocker docker' > ./data/docker-dump.sql
+# export the schema
+docker exec $DB_CONTAINER_NAME sh -c 'mariadb-dump --no-data -udocker -pdocker docker' > ./data/docker-dump.sql
+
+# add the data, ignoring caches
+docker exec $DB_CONTAINER_NAME sh -c 'mariadb-dump --no-create-info --ignore-table=docker.caches -udocker -pdocker docker' >> ./data/docker-dump.sql
