376bd096f33e7bae118652410cc35646300ad082
Julian Pollak <poljpocket@outlook.com>
2023-12-22 10:56:02 +0100

    add db recreate step to dbrestore

--- dbrestore.sh
+++ dbrestore.sh
@@ -9,4 +9,6 @@ DB_CONTAINER=_database_container_name_or_id_
 # this only works if there is only one running container with 'database' in it's name
 #DB_CONTAINER=$(docker ps --filter "name=database" --format {{.ID}} --filter status=running)
 
+docker exec -i $DB_CONTAINER sh -c 'mariadb -udocker -pdocker docker -e "DROP DATABASE docker; CREATE DATABASE docker;"'
+
 docker exec -i $DB_CONTAINER sh -c 'mariadb -udocker -pdocker docker' < ./data/database.sql
